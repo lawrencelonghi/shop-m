f8fdb30a571f9ba2a70a88af53b886967be37930
lawrencelonghi <lawlonghi@gmail.com>
2025-10-23 10:57:28 -0300

    main text changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title() }>Hi, we are&nbsp;</h1>
+        <h1 className={title() }>Welcome, we are </h1>
         <h1 className={title({ size: "lg", color:'blue' })}>
           ShopM
         </h1>
