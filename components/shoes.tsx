59471a2f19046298e0a16cb697414a12fe3376da
lawrencelonghi <lawlonghi@gmail.com>
2025-10-23 16:57:40 -0300

    shoes sction json and layout changes

--- components/shoes.tsx
+++ components/shoes.tsx
@@ -3,71 +3,61 @@ import {RadioGroup, Radio} from "@heroui/radio";
 import {Card, CardHeader, CardBody, CardFooter} from "@heroui/card";
 import { Button } from "@heroui/button";
 import Image from "next/image";
+import sneakerData from "@/data/sneakers.json" 
+
+interface SneakerProduct {
+    id: number;
+    title: string;
+    description: string;
+    price: string;
+    img: string
+}
 
 export const Shoes = () => {
-  return <div className="flex gap-6">
-      <Card isFooterBlurred>
-        <div className="relative">
-          <Image
-            alt="Card example background"
-            className="object-cover object-center"
-            src="/images/shoes/black-sneaker.jpg"
-            width={500}
-            height={800}
-          />
-        </div>
-        <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-black/50 z-10">
 
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <h4 className="text-black font-medium text-2xl">Black Sneaker</h4>
-        </CardHeader>
+const sneakers: SneakerProduct[] = sneakerData as SneakerProduct[]
 
-        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-          <div>
-            <RadioGroup color="secondary" orientation="horizontal" classNames={{ wrapper: "gap-6"}}>
-              <Radio value="small">Small</Radio>
-              <Radio value="medium">Medium</Radio>
-              <Radio value="large">Large</Radio>
-            </RadioGroup>
-          </div>
-  
-          <Button className="text-tiny" color="success" radius="full" size="sm">
-            Add to cart
-          </Button>
-        </CardFooter>
-        </div>
-      </Card>
+  return (
+    <div className="flex gap-6">
+      {sneakers.map((sneaker) => (
+          <Card key={sneaker.id} isFooterBlurred>
+            <div className="relative">
+              <Image
+                alt="Card example background"
+                src= {sneaker.img}
+                width={500}
+                height={800}
+              />
+            </div>
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-black/50 z-10">
 
-      <Card isFooterBlurred>
-        <div className="relative">
-          <Image
-            alt="Card example background"
-            className="object-cover object-center"
-            src="/images/shoes/black-sneaker.jpg"
-            width={500}
-            height={800}
-          />
-        </div>
-        <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-black/50 z-10">
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+              <h4 className="text-amber-50 font-medium text-2xl">{sneaker.title}</h4>
+            </CardHeader>
 
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <h4 className="text-black font-medium text-2xl">Black Sneaker</h4>
-        </CardHeader>
+            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+              <div>
+                {/* <RadioGroup color="secondary" orientation="horizontal" classNames={{ wrapper: "gap-6"}}>
+                  <Radio value="small">Small</Radio>
+                  <Radio value="medium">Medium</Radio>
+                  <Radio value="large">Large</Radio>
+                </RadioGroup> */}
+                <p className="text-sm">{sneaker.description}</p>
+              </div>
+             <div className="flex gap-2">
+                <Button className="text-tiny cursor-default" color="default" radius="full" size="sm">
+                  {sneaker.price}
+                </Button>
 
-        <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-          <div>
-            <RadioGroup color="secondary" orientation="horizontal" classNames={{ wrapper: "gap-6"}}>
-              <Radio value="small">Small</Radio>
-              <Radio value="medium">Medium</Radio>
-              <Radio value="large">Large</Radio>
-            </RadioGroup>
-          </div>
-  
-          <Button className="text-tiny" color="success" radius="full" size="sm">
-            Add to cart
-          </Button>
-        </CardFooter>
-        </div>
-      </Card>
-  </div>
+                <Button className="text-tiny" color="success" radius="full" size="sm">
+                  Add to cart
+                </Button>
+              </div>
+
+            </CardFooter>
+            </div>
+          </Card>
+        ))}
+    </div>
+    )
 }
